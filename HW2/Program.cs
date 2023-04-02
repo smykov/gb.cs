cba6876ff4e80d56598a9799d28a25bb48ca297b
Konstantin <smykovkn@gmail.com>
2023-04-03 00:36:45 +0700

    Задача 2

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -9,3 +9,23 @@ string? str = Console.ReadLine();
 
 Console.WriteLine(str[1]);
 
+
+Console.WriteLine("\nПрограмму, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+// Пример
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите многозвочное число: ");
+string? longStr = Console.ReadLine();
+
+if (longStr.Length < 3)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(longStr[2]);
+}
+
+
